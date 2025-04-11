fef01b0f661f0a885838f21398b405cc2321072b
scylla <131197703+acezqqq@users.noreply.github.com>
2025-04-11 15:24:06 +0200

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -168,7 +168,7 @@ local ThemeManager = {} do
 			theme[field] = Options[field].Value:ToHex()
 		end
 
-		writefile(self.Folder .. '/themes/' .. file .. '.medusa', httpService:JSONEncode(theme))
+		writefile(self.Folder .. '/themes/' .. file .. '.scylla', httpService:JSONEncode(theme))
 	end
 
 	function ThemeManager:ReloadCustomThemes()
@@ -177,10 +177,10 @@ local ThemeManager = {} do
 		local out = {}
 		for i = 1, #list do
 			local file = list[i]
-			if file:sub(-7) == '.medusa' then
+			if file:sub(-7) == '.scylla' then
 				-- i hate this but it has to be done ...
 
-				local pos = file:find('.medusa', 1, true)
+				local pos = file:find('.scylla', 1, true)
 				local char = file:sub(pos, pos)
 
 				while char ~= '/' and char ~= '\\' and char ~= '' do
